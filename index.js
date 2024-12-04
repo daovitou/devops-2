2bdb9437fe4ad02c186ba4d81f7739d2445fe193
vitoudao <vitoudao@gmail.com>
2024-12-04 15:34:05 +0700

    TestBuild6

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const app = require('./app.js');
 
+app.get("/testing",(req,res,next)=>{
+    try {
+        return res.status(200).send("Testing route")
+    } catch (error) {
+        return res.status(500).send("Server error")
+    }
+})
+
 app.listen(8081, () => {
     console.log("Running on 8081")
 })
