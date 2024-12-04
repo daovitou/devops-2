de13f0ba61f7e9a6f34a0488ad4bf59d99c72d60
vitoudao <vitoudao@gmail.com>
2024-12-04 15:56:59 +0700

    build4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./app.js');
 
 app.get("/testing",(req,res,next)=>{
     try {
-        return res.status(200).send("Testing route 123456")
+        return res.status(200).send("Testing route 123456!@#")
     } catch (error) {
         return res.status(500).send("Server error")
     }
